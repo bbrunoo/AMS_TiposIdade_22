ca2aa58825a7a620eb473de19d48ee45673b0855
bbrunoo <brunoedsanntos@gmail.com>
2022-05-29 13:17:13 -0300

    TiposIdade

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using static System.Console;
+// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+   
+int idadeMin = 0;
+int idadeMax = 0;
+int mediaAluno = 0;
+
+WriteLine("Escreva a primeira idade");
+idadeMin = Convert.ToInt32(ReadLine());
+
+WriteLine("Escreva a segunda idade");
+idadeMax = Convert.ToInt32(ReadLine());
+
+mediaAluno = (idadeMax + idadeMin) / 2;
+
+WriteLine($"A media da faculdade é: {mediaAluno}");
+ 
+
+
